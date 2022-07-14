8d9c7d9c243a30691f4da9b38b5e270777c6ddaf
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2022-07-14 20:51:26 +0100

    Longest Palindromic String

--- Day_14.py
+++ Day_14.py
@@ -1,29 +1,29 @@
 #Given a string s, return the longest palindromic substring in s.
-    def longestPalindrome(self, s: str) -> str:
+def longestPalindrome(self, s: str) -> str:
         
-        pal = ""
-        palLen = 0
-        #ababbd
-        for i in range(0,len(s)):
-            #odd cases
-            l,r = i, i
+    pal = ""
+    palLen = 0
+    #ababbd
+    for i in range(0,len(s)):
+        #odd cases
+        l,r = i, i
+        
+        while l >= 0 and r < len(s) and s[l]==s[r]:
+            if (r - l + 1) > palLen:
+                pal= s[l:r+1]
+                palLen = r - l + 1
+            l-=1
+            r+=1
+        #even cases
+        
+        l, r = i, i+1
+        while l >= 0 and r < len(s) and s[l]==s[r]:
+            if (r - l + 1) > palLen:
+                pal= s[l:r+1]
+                palLen = r - l + 1
+            l-=1
+            r+=1
+    return pal
+        
             
-            while l >= 0 and r < len(s) and s[l]==s[r]:
-                if (r - l + 1) > palLen:
-                    pal= s[l:r+1]
-                    palLen = r - l + 1
-                l-=1
-                r+=1
-            #even cases
-            
-            l, r = i, i+1
-            while l >= 0 and r < len(s) and s[l]==s[r]:
-                if (r - l + 1) > palLen:
-                    pal= s[l:r+1]
-                    palLen = r - l + 1
-                l-=1
-                r+=1
-        return pal
-            
-                
-      
+    
