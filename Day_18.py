67521e21d6338690eb788b9ee30a6478c5d2d55d
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2022-07-28 21:55:32 +0100

    Valid BST

---
+++ Day_18.py
@@ -0,0 +1,13 @@
+# Validate BST
+#Check if a tree is a valid Binary Search Tree
+
+def isValid( root):
+    if root is None:
+        return True
+    def dfs(left,node,right):
+        if not(node.val > left and node.val < right):
+            return False
+        else:
+            return dfs(left,node.left,node.val) and dfs(node.right,node,right)
+    return dfs(root, float("-inf"), float("inf"))
+
